4f059150446f217d60ffd0c5c420be9aace629ae
Juno Joseph <87766026+Juno-Joseph@users.noreply.github.com>
2024-11-27 03:08:56 -0500

    1st Step
    Adding all assets, Created animated BG, finish the anim Bg

---
+++ Scripts/Pochita.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+@onready var anim = $AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	anim.play("Idle")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
