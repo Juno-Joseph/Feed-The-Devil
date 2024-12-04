103d71b46ec3f6b4494b92661a6515b43551849c
Juno Joseph <87766026+Juno-Joseph@users.noreply.github.com>
2024-12-04 01:02:36 -0500

    New drag and drop
    Removed inventory feature, using drag and drop for each food item.

--- Scripts/GUI/inventory_slot.gd
+++
@@ -1,9 +0,0 @@
-#This alows us to create a new node in GoDot, giving us 5 slots for food
-class_name InventorySlot
-extends PanelContainer 
-
-@export var type: FoodData.Type
-
-func init(t: FoodData.Type, cms: Vector2) -> void:
-	type = t
-	custom_minimum_size = cms
