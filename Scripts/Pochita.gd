fb585a4a4b6a18903712c45191d8820209af0203
Juno Joseph <87766026+Juno-Joseph@users.noreply.github.com>
2024-12-08 17:21:32 -0500

    adding the rest of the food

--- Scripts/Pochita.gd
+++ Scripts/Pochita.gd
@@ -6,12 +6,9 @@ extends CharacterBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	anim.play("Idle")
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	#if global.is_dragging:
-	#	visible = true
-	#else:
-	#	visible = false
 	pass
