67b62ef68c64fb7b2ba78d24c613d4e3064f0190
Juno Joseph <87766026+Juno-Joseph@users.noreply.github.com>
2025-05-14 23:11:04 -0400

    Did CSM animation :D

--- Scripts/Cut Scenes/csm.gd
+++ Scripts/Cut Scenes/csm.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
-##uncomment this when cutscene is finished
-##@onready var anim = $AnimatedSprite2D
+@onready var anim = $AnimatedSprite2D
 @onready var transition = $Transition
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	##anim.play("default")
+	anim.play("Slashes")
 	transition.play("dark_fade_in")
+
+func _on_animated_sprite_2d_animation_finished():
+	anim.play("CSM")
+	
