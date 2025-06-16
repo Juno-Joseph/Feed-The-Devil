659fc35c2f852aa0dafa20669a98b17349b3bb47
Juno Joseph <87766026+Juno-Joseph@users.noreply.github.com>
2025-06-16 00:27:02 -0400

    added alley cutscene

--- Scripts/Cut Scenes/memories.gd
+++ Scripts/Cut Scenes/memories.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
-##uncomment this when cutscene is finished
-##@onready var anim = $AnimatedSprite2D
+@onready var anim = $AnimatedSprite2D
 @onready var transition = $Transition
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	##anim.play("default")
+	anim.play("alley")
 	transition.play("light_fade_in")
+
+func _on_animated_sprite_2d_animation_finished():
+	pass # Replace with function body.
